cd92ffb1cb08a210e1eb77e10c2825c1a23d99a1
Zoe Rowbotham <zoe2.rowbotham@live.uwe.ac.uk>
2021-10-16 16:20:48 +0100

    Less Floaty Jump

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -3,7 +3,7 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-	public const float GROUNDED_RADIUS = 0.2f;
+	public const float GROUNDED_RADIUS = 0.05f;
 	public const float CEILING_RADIUS = 0.05f;
 
 	[Header("References")]
@@ -12,56 +12,35 @@ public class PlayerMovement : MonoBehaviour
 	[SerializeField] private Transform m_ceilingCheck = default;
 	[SerializeField] private Collider2D m_crouchDisableCollider = default;
 
-	[Header("Configuration Values")]
-	[SerializeField] private float m_runSpeed = 40f;
-	[SerializeField] private float m_jumpForce = 400f;
-	[Range(0, 1)] [SerializeField] private float m_crouchSpeedMultiplier = 0.36f;
+	[Header("Movement Configuration Values")]
+	[SerializeField] private float m_runSpeed = 400f;
 	[Range(0, 0.3f)] [SerializeField] private float m_movementSmoothingAmount = 0.05f;
+
+	[Header("Jumping Configuration Values")]
+	[SerializeField] private float m_jumpForce = 400f;
 	[SerializeField] private bool m_canAirControl = false;
+	[SerializeField] private float m_fallMultiplier = 2.5f;
+	[SerializeField] private float m_lowJumpMultiplier = 2f;
+	[SerializeField] private float m_lastGroundedDelay;
 	[SerializeField] private LayerMask m_whatIsGround;
 
-	private bool m_isGrounded;
+	[Header("Crouching Configuration Values")]
+	[Range(0, 1)] [SerializeField] private float m_crouchSpeedMultiplier = 0.36f;
+
+	private bool m_isGrounded = false;
 	private bool m_isFacingRight = true;
 	private Vector3 m_velocity = Vector3.zero;
 
 	private float m_movementInput = 0;
+	private bool m_jumpInput = false;
 	private bool m_crouchingInput = false;
 
-	private bool m_isJumping = false;
 	private bool m_wasCrouching = false;
-
-    private void FixedUpdate()
-	{
-		bool wasGrounded = m_isGrounded;
-		m_isGrounded = false;
-
-		// Ground Check
-		Collider2D[] colliders = Physics2D.OverlapCircleAll(m_groundCheck.position, GROUNDED_RADIUS, m_whatIsGround);
-		for (int i = 0; i < colliders.Length; i++)
-		{
-			if (colliders[i].gameObject != gameObject)
-			{
-				m_isGrounded = true;
-				m_isJumping = false;
-			}
-		}
-	}
-
-	private void Update()
-	{
-		float moveDir = m_movementInput * m_runSpeed * Time.fixedDeltaTime;
-
-		Move(moveDir, m_crouchingInput);
-	}
+	private float m_lastTimeGrounded = 0;
 
 	public void HandleJump(InputAction.CallbackContext context)
 	{
-		if (m_isGrounded && !m_isJumping)
-		{
-			m_isGrounded = false;
-			m_isJumping = true;
-			m_rigidbody.AddForce(new Vector2(0f, m_jumpForce));
-		}
+		m_jumpInput = context.ReadValue<float>() > 0;
 	}
 
 	public void HandleCrouch(InputAction.CallbackContext context)
@@ -78,8 +57,30 @@ public class PlayerMovement : MonoBehaviour
 	{
 		m_movementInput = context.ReadValue<float>();
 	}
+	private void FixedUpdate()
+	{
+		bool wasGrounded = m_isGrounded;
+		m_isGrounded = false;
 
-	public void Move(float move, bool crouch)
+		// Ground Check
+		Collider2D[] colliders = Physics2D.OverlapCircleAll(m_groundCheck.position, GROUNDED_RADIUS, m_whatIsGround);
+		for (int i = 0; i < colliders.Length; i++)
+		{
+			if (colliders[i].gameObject != gameObject)
+			{
+				m_isGrounded = true;
+			}
+		}
+	}
+
+	private void Update()
+	{
+		float moveDir = m_movementInput * m_runSpeed * Time.fixedDeltaTime;
+
+		Move(moveDir, m_crouchingInput, m_jumpInput);
+	}
+
+	public void Move(float move, bool crouch, bool jump)
 	{
 		// If crouching, check to see if the character can stand up
 		if (!crouch)
@@ -128,6 +129,12 @@ public class PlayerMovement : MonoBehaviour
 			{
 				Flip();
 			}
+		}
+
+		if (m_isGrounded && jump)
+		{
+			m_isGrounded = false;
+			m_rigidbody.AddForce(new Vector2(0f, m_jumpForce));
 		}
 	}
 
