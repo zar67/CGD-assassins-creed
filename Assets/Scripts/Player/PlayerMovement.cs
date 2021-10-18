5f472cd46a6f480f254b7d87d561b99c64e982ea
Zoe Rowbotham <zoe2.rowbotham@live.uwe.ac.uk>
2021-10-18 11:19:55 +0100

    Better Movement and Better Wall Jump

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,112 +5,159 @@ public class PlayerMovement : MonoBehaviour
 {
 	public const float GROUNDED_RADIUS = 0.05f;
 	public const float CEILING_RADIUS = 0.05f;
+	public const float WALL_GRABBING_RADIUS = 0.05f;
 
 	[Header("References")]
 	[SerializeField] private Rigidbody2D m_rigidbody = default;
-	[SerializeField] private PlayerWallSlidingCheck m_wallCheck = default;
 	[SerializeField] private Transform m_groundCheck = default;
 	[SerializeField] private Transform m_ceilingCheck = default;
-	[SerializeField] private Collider2D m_crouchDisableCollider = default;
+	[SerializeField] private Transform m_wallCheck = default;
+	[SerializeField] private Collider2D m_crouchCollider = default;
 
 	[Header("Movement Configuration Values")]
 	[SerializeField] private float m_runSpeed = 400f;
 	[Range(0, 0.3f)] [SerializeField] private float m_movementSmoothingAmount = 0.05f;
 
 	[Header("Jumping Configuration Values")]
+	[SerializeField] private float m_jumpInputThreshold = 0f;
 	[SerializeField] private float m_jumpForce = 400f;
 	[SerializeField] private bool m_canAirControl = false;
 	[SerializeField] private float m_fallMultiplier = 2.5f;
 	[SerializeField] private float m_lowJumpMultiplier = 2f;
 	[SerializeField] private LayerMask m_whatIsGround;
 
+	[Header("Wall Jumping Configuration Values")]
+	[SerializeField] private float m_wallGrabbingGravityMultiplier = 0.7f;
+	[SerializeField] private float m_wallJumpDuration = 0.2f;
+	[SerializeField] private float m_wallJumpHeightMultiplier = 1.5f;
+	[SerializeField] private LayerMask m_whatIsWall;
+
 	[Header("Crouching Configuration Values")]
+	[SerializeField] private float m_crouchInputThreshold = 0.8f;
 	[Range(0, 1)] [SerializeField] private float m_crouchSpeedMultiplier = 0.36f;
 
-	private bool m_isGrounded = false;
-	private bool m_isFacingRight = true;
-	private Vector3 m_velocity = Vector3.zero;
+	[SerializeField] private bool m_canControl = true;
 
-	private float m_movementInput = 0;
-	private bool m_jumpInput = false;
-	private bool m_crouchingInput = false;
+	[SerializeField] private bool m_isGrounded = false;
+	[SerializeField] private bool m_isFacingRight = true;
+	[SerializeField] private Vector3 m_velocity = Vector3.zero;
 
-	private bool m_wasCrouching = false;
+	[SerializeField] private float m_movementInput = 0;
+	[SerializeField] private bool m_jumpInput = false;
+	[SerializeField] private bool m_jumpInputPressedThisFrame = false;
+	[SerializeField] private bool m_crouchingInput = false;
 
-	public void HandleJump(InputAction.CallbackContext context)
+	[SerializeField] private bool m_wasCrouching = false;
+
+	[SerializeField] private bool m_canGrabWall = false;
+	[SerializeField] private bool m_isGrabbingWall = false;
+
+	[SerializeField] private float m_wallJumpTimer = 0;
+
+	[SerializeField] private float m_defaultGravityScale;
+
+    private void Awake()
+    {
+		m_defaultGravityScale = m_rigidbody.gravityScale;
+    }
+
+    public void HandleJumpInput(InputAction.CallbackContext context)
 	{
-		m_jumpInput = context.ReadValue<float>() > 0;
+		m_jumpInput = context.ReadValue<float>() > m_jumpInputThreshold;
+		m_jumpInputPressedThisFrame = context.performed;
 	}
 
-	public void HandleCrouch(InputAction.CallbackContext context)
+	public void HandleCrouchInput(InputAction.CallbackContext context)
 	{
-		m_crouchingInput = context.ReadValue<float>() > 0.8f;
+		m_crouchingInput = context.ReadValue<float>() > m_crouchInputThreshold;
 	}
 
-	public void HandleClimb(InputAction.CallbackContext context)
+	public void HandleClimbInput(InputAction.CallbackContext context)
 	{
 
 	}
 
-	public void HandleMovement(InputAction.CallbackContext context)
+	public void HandleMovementInput(InputAction.CallbackContext context)
 	{
 		m_movementInput = context.ReadValue<float>();
 	}
+
 	private void FixedUpdate()
 	{
 		bool wasGrounded = m_isGrounded;
 		m_isGrounded = false;
 
 		// Ground Check
-		Collider2D[] colliders = Physics2D.OverlapCircleAll(m_groundCheck.position, GROUNDED_RADIUS, m_whatIsGround);
-		for (int i = 0; i < colliders.Length; i++)
+		m_isGrounded = Physics2D.OverlapCircle(m_groundCheck.position, GROUNDED_RADIUS, m_whatIsGround);
+		
+		// Crouch Check
+		if (m_wasCrouching && !m_crouchingInput)
 		{
-			if (colliders[i].gameObject != gameObject)
+			if (Physics2D.OverlapCircle(m_ceilingCheck.position, CEILING_RADIUS, m_whatIsGround))
 			{
-				m_isGrounded = true;
+				m_crouchingInput = true;
 			}
 		}
+
+		// Wall Grabbing Check
+		m_canGrabWall = Physics2D.OverlapCircle(m_wallCheck.position, WALL_GRABBING_RADIUS, m_whatIsWall);
 	}
 
 	private void Update()
 	{
-		float moveDir = m_movementInput * m_runSpeed * Time.fixedDeltaTime;
-
-		Move(moveDir, m_crouchingInput, m_jumpInput);
-	}
-
-	public void Move(float move, bool crouch, bool jump)
-	{
-		// If crouching, check to see if the character can stand up
-		if (!crouch)
+		if (m_canControl)
 		{
-			if (Physics2D.OverlapCircle(m_ceilingCheck.position, CEILING_RADIUS, m_whatIsGround))
+			float moveDir = m_movementInput * m_runSpeed * Time.deltaTime;
+
+			UpdateCrouching(ref moveDir);
+			UpdateWallJumping(ref moveDir);
+			UpdateJumping(ref moveDir);
+
+			UpdateMovement(moveDir);
+		}
+		else
+		{
+			m_wallJumpTimer -= Time.deltaTime;
+			if (m_wallJumpTimer <= 0)
 			{
-				crouch = true;
+				m_wallJumpTimer = 0;
+				m_canControl = true;
 			}
 		}
 
+		// Check if we are facing the right direction
+		if ((m_movementInput > 0 && !m_isFacingRight) ||
+			(m_movementInput < 0 && m_isFacingRight))
+		{
+			Flip();
+		}
+
+		m_jumpInputPressedThisFrame = false;
+	}
+
+	private void UpdateCrouching(ref float movement)
+	{
 		if (m_isGrounded || m_canAirControl)
 		{
-			if (crouch)
+			if (m_crouchingInput)
 			{
 				if (!m_wasCrouching)
 				{
 					m_wasCrouching = true;
 				}
 
-				move *= m_crouchSpeedMultiplier;
+				movement *= m_crouchSpeedMultiplier;
 
-				if (m_crouchDisableCollider != null)
+				if (m_crouchCollider != null)
 				{
-					m_crouchDisableCollider.enabled = false;
+					m_crouchCollider.enabled = false;
 				}
 			}
 			else
 			{
-				if (m_crouchDisableCollider != null)
+				if (m_crouchCollider != null)
 				{
-					m_crouchDisableCollider.enabled = true;
+					m_crouchCollider.enabled = true;
 				}
 
 				if (m_wasCrouching)
@@ -118,35 +165,68 @@ public class PlayerMovement : MonoBehaviour
 					m_wasCrouching = false;
 				}
 			}
+		}
+	}
 
-			Vector3 targetVelocity = new Vector2(move * 10f, m_rigidbody.velocity.y);
-			m_rigidbody.velocity = Vector3.SmoothDamp(m_rigidbody.velocity, targetVelocity, ref m_velocity, m_movementSmoothingAmount);
+	private void UpdateWallJumping(ref float movement)
+	{
+		bool wasGrabbingWall = m_isGrabbingWall;
 
-			// Check if we are facing the right direction
-			if ((move > 0 && !m_isFacingRight) ||
-				(move < 0 && m_isFacingRight))
+		m_isGrabbingWall = false;
+		if (m_canGrabWall && !m_isGrounded)
+		{
+			if ((m_isFacingRight && movement > 0) || (!m_isFacingRight && movement < 0))
 			{
-				Flip();
+				m_isGrabbingWall = true;
 			}
 		}
 
-		if (jump && m_isGrounded)
+		if (m_isGrabbingWall)
+		{
+			m_rigidbody.gravityScale = m_defaultGravityScale * m_wallGrabbingGravityMultiplier;
+			m_rigidbody.velocity = Vector2.zero;
+
+			if (m_jumpInput && m_jumpInputPressedThisFrame)
+			{
+				m_wallJumpTimer = m_wallJumpDuration;
+				m_rigidbody.AddForce(new Vector2(-(m_movementInput * m_jumpForce), m_jumpForce * m_wallJumpHeightMultiplier));
+				Flip();
+
+				m_rigidbody.gravityScale = m_defaultGravityScale;
+				m_isGrabbingWall = false;
+				m_canControl = false;
+			}
+		}
+		else
+		{
+			m_rigidbody.gravityScale = m_defaultGravityScale;
+		}
+	}
+
+	private void UpdateJumping(ref float movement)
+	{
+		if (m_jumpInput && m_isGrounded)
 		{
 			m_isGrounded = false;
 			m_rigidbody.AddForce(new Vector2(0f, m_jumpForce));
-		}
-		else if (jump && !m_isGrounded && m_wallCheck.CollidingWithWall())
-		{
-			m_rigidbody.AddForce(new Vector2(-m_jumpForce * 0.5f, m_jumpForce * 0.5f));
 		}
 
 		if (m_rigidbody.velocity.y < 0)
 		{
 			m_rigidbody.velocity += Vector2.up * Physics2D.gravity * (m_fallMultiplier - 1) * Time.deltaTime;
 		}
-		else if (m_rigidbody.velocity.y > 0 && !jump)
+		else if (m_rigidbody.velocity.y > 0 && !m_jumpInput)
 		{
 			m_rigidbody.velocity += Vector2.up * Physics2D.gravity * (m_lowJumpMultiplier - 1) * Time.deltaTime;
+		}
+	}
+
+	public void UpdateMovement(float move)
+	{
+		if (m_isGrounded || m_canAirControl)
+		{
+			Vector3 targetVelocity = new Vector2(move * 10f, m_rigidbody.velocity.y);
+			m_rigidbody.velocity = Vector3.SmoothDamp(m_rigidbody.velocity, targetVelocity, ref m_velocity, m_movementSmoothingAmount);
 		}
 	}
 
