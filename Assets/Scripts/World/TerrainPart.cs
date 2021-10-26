996a0bd7253255ca7334f7c185b40fc8d824cb5e
Stan <stanley.samson.davis@gmail.com>
2021-10-26 15:31:24 +0100

    Improved Terrain Part selection
    - LOW, MID, and HIGH choices added so parts are only selected based on compatibility with the previous part

--- Assets/Scripts/World/TerrainPart.cs
+++ Assets/Scripts/World/TerrainPart.cs
@@ -5,4 +5,38 @@ using UnityEngine;
 public class TerrainPart : MonoBehaviour
 {
     public Transform endPosition;
+    public TerrainHeight terrainHeight;
+
+    public bool Compare(TerrainHeight other)
+    {
+        //if low return mid or low
+        //if mid return low or high
+        //if high return mid or high
+
+        switch (terrainHeight)
+        {
+            case TerrainHeight.LOW:
+                if (other == TerrainHeight.LOW) return true;
+                if (other == TerrainHeight.MID) return true;
+                else return false;
+            case TerrainHeight.MID:
+                if (other == TerrainHeight.LOW) return true;
+                if (other == TerrainHeight.MID) return true;
+                if (other == TerrainHeight.HIGH) return true;
+                else return false;
+            case TerrainHeight.HIGH:
+                if (other == TerrainHeight.MID) return true;
+                if (other == TerrainHeight.HIGH) return true;
+                else return false;
+        }
+
+        return false;
+    }
+}
+
+public enum TerrainHeight
+{
+    HIGH,
+    MID,
+    LOW,
 }
